9b1eeefec15a4bc4917e074e108b02b045edd112
kamborges <kamarianiborges@gmail.com>
2025-10-04 14:20:15 -0300

    ajustando rota para a aplicação rodar em npm start

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 
-const routes = require('./src/routes')
+const routes = require('./routes/index')
 
 const app = express();
 const hostname = '127.0.0.1';
