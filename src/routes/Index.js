bcf86b2e7fa50c3fa36eadbfb935359913c4030c
kamborges <kamarianiborges@gmail.com>
2025-10-04 14:19:24 -0300

    Incluindo rotas para order e store

--- src/routes/Index.js
+++ src/routes/Index.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const userRoutes = require("./userRoutes");
 const productRoutes = require("./productRoutes");
+const storeRoutes = require("./storeRoutes");
+const orderRoutes = require("./orderRoutes");
 
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
+router.use("/stores", storeRoutes);
+router.use("/orders", orderRoutes);
 
 module.exports = router;
+
+
+
